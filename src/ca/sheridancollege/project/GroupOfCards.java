46b3402dfc5f85f32df7b4d6743200a2a322f1fe
Suman Dhungel <dhungel@sheridancollege.ca>
2025-08-06 11:46:27 -0400

    Updated group of Cards

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -7,22 +7,28 @@ package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * A concrete class that represents any grouping of cards for a Game. HINT, you
+ * might want to subclass this more than
+ * once. The group of cards has a maximum size attribute which is flexible for
+ * reuse.
+ * 
+ * @author Prabmehak Singh
+ * @author Suman Dhungel
+ * @author Hammed Seehar
+ * @author Chris Jude Stellus Aug 2025
  */
 public class GroupOfCards {
 
-    //The group of cards, stored in an ArrayList
+    // The group of cards, stored in an ArrayList
     private ArrayList<Card> cards;
-    private int size;//the size of the grouping
+    private int size;// the size of the grouping
 
     public GroupOfCards(int size) {
         this.size = size;
+        this.cards = new ArrayList<>();
     }
 
     /**
@@ -46,10 +52,61 @@ public class GroupOfCards {
     }
 
     /**
+     * @return the current number of cards in the group
+     */
+    public int getCardCount() {
+        return cards.size();
+    }
+
+    /**
      * @param size the max size for the group of cards
      */
     public void setSize(int size) {
         this.size = size;
     }
 
-}//end class
+    /**
+     * Adds a single card to the top (end) of the group.
+     *
+     * @param card The card to add.
+     */
+    public void addCard(Card card) {
+        if (card != null) {
+            this.cards.add(card);
+        }
+    }
+
+    /**
+     * Adds a collection of cards to the bottom (end) of the group.
+     * This is useful for adding won cards to a player's deck.
+     *
+     * @param cardsToAdd The list of cards to add.
+     */
+    public void addCardsBottom(List<Card> cardsToAdd) {
+        if (cardsToAdd != null && !cardsToAdd.isEmpty()) {
+            this.cards.addAll(cardsToAdd);
+        }
+    }
+
+    /**
+     * Removes and returns the top card from the group.
+     *
+     * @return The top card, or null if the group is empty.
+     */
+    public Card removeTopCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0); // Remove from the "top" of the deck
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the group of cards is empty.
+     *
+     * @return true if the group has no cards, false otherwise.
+     */
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+}
