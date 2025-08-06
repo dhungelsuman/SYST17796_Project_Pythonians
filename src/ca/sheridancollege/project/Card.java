fdd692c966b78596a268582c842c8191bf9a01da
Suman Dhungel <dhungel@sheridancollege.ca>
2025-08-06 11:45:18 -0400

    cahnged the card java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,7 +1,10 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * @author Prabmehak Singh
+ * @author Suman Dhungel
+ * @author Hammed Seehar
+ * @author Chris Jude Stellus August 2025
  */
 package ca.sheridancollege.project;
 
@@ -9,7 +12,6 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author dancye
  */
 public abstract class Card {
     //default modifier for child classes
